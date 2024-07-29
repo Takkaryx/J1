4476559b908819c022afe2598ebfaee828c96bed
Janos Cserna <JanosCserna@gmail.com>
2024-07-28 18:15:32 -0700

    some fixes

--- j1-host/target/thumbv7em-none-eabihf/debug/deps/j1_host-c5bb34127cce9d50.d
+++
@@ -1,5 +0,0 @@
-/Users/janoscserna/Documents/code/j1/j1-host/target/thumbv7em-none-eabihf/debug/deps/libj1_host-c5bb34127cce9d50.rmeta: src/main.rs
-
-/Users/janoscserna/Documents/code/j1/j1-host/target/thumbv7em-none-eabihf/debug/deps/j1_host-c5bb34127cce9d50.d: src/main.rs
-
-src/main.rs:
